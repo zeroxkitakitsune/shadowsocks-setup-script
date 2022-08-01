4b5484cf47bb015b3a1ab3a7078b5c2d567e79f2
zeroxkitakitsune <kitakitsunee@proton.me>
2022-08-01 11:09:28 -0400

    Initial Shadowsocks Script commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# shadowsocks-setup-script
-Bash script to setup shadowsocks server
+# Shadowsocks Setup Script
+## Recommended system requirements
+- Ubuntu(20.04)
+## Recommended steps
+1. Launch as root
+1. Upgrade your system
+    - `sudo apt update && apt upgrade -y`
+1. Install snapd(if not installed)
+    - `sudo apt install -y snapd`
+1. Reboot and wait for couple seconds
+    - `sudo reboot`
+1. Add script
+    - `nano ss-setup.sh`
+1. Copy and paste all the data from `ss-setup.sh`
+1. Replace `<server_ip>` with your server ip
+1. Replace `<password>` with your password you're going to use to connect to your shadowsocks server
+1. Run `bash ss-setup.sh` ***Note: the server will be rebooted***
+
+## Additional commands
+- To check if your shadowsocks server is running use `sudo systemctl status shadowsocks-libev-server@config`
+
---
+++ ss-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+snap install shadowsocks-libev
+mkdir -p /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev
+echo "{
+    \"server\":\"<server_ip>\",
+    \"server_port\":443,
+    \"local_port\":1080,
+    \"password\":\"<password>\",
+    \"timeout\":20,
+    \"method\":\"chacha20-ietf-poly1305\",
+    \"fast_open\":true,
+    \"nameserver\":\"1.1.1.1\",
+    \"mode\":\"tcp_and_udp\"
+}" >> /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
+echo "
+[Unit]
+Description=Shadowsocks-Libev Custom Server Service for %I
+Documentation=man:ss-server(1)
+After=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/snap run shadowsocks-libev.ss-server -c /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/%i.json
+
+[Install]
+WantedBy=multi-user.target
+" >> /etc/systemd/system/shadowsocks-libev-server@.service
+sudo systemctl enable --now shadowsocks-libev-server@config
+reboot
